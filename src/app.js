4393b4fb6364818f897e433703006ad1cdfcf82f
Joseph Hensersky <josephhensersky@gmail.com>
2020-03-12 20:24:18 -0700

    adds basic file uploading

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const PORT = 8080;
 const HOST = '0.0.0.0';
 const app = express();
 
 app.set('view engine', 'hbs');
 app.set('view options', { layout: 'layout.hbs' });
+app.use(fileUpload());
 
 app.get('/', (req, res) => {
-  res.render('index', {})
+  res.render('index', {});
+});
+
+app.post('/upload', function(req, res) {
+  req.files.image.mv(`/tmp/tmp/${req.files.image.name}`, function(err) {
+    if (err)
+      return res.status(500).send(err);
+    res.render('upload', {filename: req.body.filename});
+  });
 });
 
 app.listen(PORT, HOST);
