4c7e11bc06f956e721f640365fcc3a8b631884e4
Joseph Hensersky <josephhensersky@gmail.com>
2020-03-13 17:49:13 -0700

    adds full web functionality

--- src/app.js
+++ src/app.js
@@ -1,23 +1,52 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const fs = require('fs');
+const { exec } = require("child_process");
 const PORT = 8080;
 const HOST = '0.0.0.0';
+const IMAGE_DIRECTORY = '/tmp/tmp/';
+const MOD_IMAGE_DIRECTORY = `${IMAGE_DIRECTORY}resized/`;
 const app = express();
 
 app.set('view engine', 'hbs');
 app.set('view options', { layout: 'layout.hbs' });
-app.use(fileUpload());
+app.use(fileUpload({
+    useTempFiles : true,
+    tempFileDir : IMAGE_DIRECTORY
+}));
 
 app.get('/', (req, res) => {
-  res.render('index', {});
+  fs.readdir(MOD_IMAGE_DIRECTORY, function (err, files) {
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    }
+    res.render('index', {images:files, imageDir:MOD_IMAGE_DIRECTORY});
+  });
 });
 
-app.post('/upload', function(req, res) {
-  req.files.image.mv(`/tmp/tmp/${req.files.image.name}`, function(err) {
+app.get('/download-image', (req, res) => {
+  res.download(`${req.query.dir}${req.query.file}`, req.query.file, (err) => {
     if (err)
-      return res.status(500).send(err);
-    res.render('upload', {filename: req.body.filename});
+      console.log("ERROR: Sending image");
   });
+});
+
+app.post('/upload-image', function(req, res) {
+  // req.body.filename = filename
+  // req.body.image = image
+  let filename = req.body.filename && req.body.filename !== '' ? req.body.filename : req.files.image.name;
+  exec(`./src/image_resize.py ${req.files.image.tempFilePath} ${MOD_IMAGE_DIRECTORY}${filename}`, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+  res.render('upload', {filename: filename});
 });
 
 app.listen(PORT, HOST);
---
+++ src/image_resize.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Joseph Hensersky
+Program for cropping image into a square
+"""
+import sys
+from PIL import Image
+
+FIND_CENTER = lambda x, y: int((x-y)/2) # for finding location to paste image in created square
+
+def crop_image(img):
+    """Return cropped image object."""
+    fill_color = (0, 0, 0, 0) # black background
+    width, height = img.width, img.height
+    size = max(width, height)
+    square_img = Image.new('RGBA', (size, size), fill_color)
+    square_img.paste(img, (FIND_CENTER(size, width), FIND_CENTER(size, height)))
+    return square_img
+
+def main(image_file, out_file):
+    """Open image file and save new, cropped image."""
+    og_image = Image.open(image_file)
+    new_image = crop_image(og_image)
+    new_image.save(out_file, og_image.format)
+
+if __name__ == "__main__":
+    IMAGE_NAME = ""
+    OUTPUT_NAME = ""
+    try:
+        IMAGE_NAME = sys.argv[1]
+        OUTPUT_NAME = sys.argv[2]
+    except IndexError:
+        print("Usage: ./image_resize.py image_name output_file")
+        sys.exit(1)
+
+    main(IMAGE_NAME, OUTPUT_NAME)
